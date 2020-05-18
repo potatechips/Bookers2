7972081de837b44297c5795391265911f5b99507
vagrant <vagrant@localhost.localdomain>
2020-05-18 13:43:40 +0000

    first-commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,18 @@
+class BooksController < ApplicationController
+
+	def top
+    end
+
+	def new
+    end
+
+    def create
+    end
+
+    def index
+    end
+
+    def show
+    end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'books#top'
+  get 'top' => 'books#top'
+
+
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books, only: [:new, :create, :index, :show]
+
+end
